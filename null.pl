b1ed7585b071f57fb78e1e0853bb25b88ecc293a
Demarcus Peeked <92762298+popbobFAN@users.noreply.github.com>
2022-10-21 04:38:46 -0400

    Create null.pl

---
+++ null.pl
@@ -0,0 +1,21 @@
+while(null == null){
+  #MAKE SURE THE NULL IS A NULL....
+  if(null == null){
+    print "MAJOR ALERT SYSTEM SHUTDOWN IMMEDIANTLY A NULL HAS BEEN NULLED AND DETECTED.....";
+    print "NULL ALERT!";
+        print "NULL ALERT!";
+            print "NULL ALERT!";
+                print "NULL ALERT!";
+                    print "NULL ALERT!";
+                        print "NULL ALERT!";
+                            print "NULL ALERT!";
+                                print "NULL ALERT!";
+                                    print "NULL ALERT!";
+                                        print "NULL ALERT!";
+                                            print "NULL ALERT!";
+                                                print "NULL ALERT!";
+                                                    print "NULL ALERT!";
+                                                        print "NULL ALERT!";
+                                                        return;
+  }
+}
